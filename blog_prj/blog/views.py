7783d9028f1f3aa6af263654ac6d1a1962a7f2c5
Veaceslav Zagaevschi <v.zagaevschi@gmail.com>
2022-11-06 13:17:07 +0200

    Refactoring. Corrected templates place

--- blog_prj/blog/views.py
+++ blog_prj/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'blog/index.html'
+        )
